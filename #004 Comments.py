bed6cf30ee8ee4be5512cb61ed4be5ae890b3a4e
Abdullah_Maghawry <abdalabadwy3@gmail.com>
2026-06-19 16:28:39 +0300

    004 lesson: Comments

---
+++ #004 Comments.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------------
+# information about the file 
+# license
+# who created the file
+# when the file was created
+# why the file was created
+# write beside the programming line
+# write before the programming line
+# prevent code from running
+#-----------------------------------------------------
+print("Hello python")
+print("programming")
+# print("abdallah")       # Use Ctrl + / to comment or uncomment a line.
+
+'''
+THIS IS 
+NOT A MULTILINE
+COMMENT
+IT IS A STRING
+BUT NOT DEFINED TO ANY VARIABLE
+'''
