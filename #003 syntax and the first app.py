28680cea28e6a416cf8c0c722d7a9b143521b554
Abdullah_Maghawry <abdalabadwy3@gmail.com>
2026-06-19 16:38:53 +0300

    syntax and the first app

--- #003 syntax and the first app.py
+++ #003 syntax and the first app.py
@@ -1,2 +1,2 @@
-print("Hello python") ; print("programming") # You can write multiple statements in a single line by separating them with a semicolon (;).
+print("Hello python"); print("programming") # You can write multiple statements in a single line by separating them with a semicolon (;).
 print("Abdullah is the best student")
