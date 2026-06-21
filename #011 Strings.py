a0485e87b27fd323b1602ab86d99cadbd8ec58a1
Abdullah_Maghawry <abdalabadwy3@gmail.com>
2026-06-21 07:46:17 +0300

    Strings[1]

---
+++ #011 Strings.py
@@ -0,0 +1,28 @@
+#-------------------------
+#----Strings----
+#-------------------------
+
+
+mystringone = "iam abdallah ahmed"
+mystringtwo = 'iam abdallah ahmed'
+
+print(mystringone)  # Output: iam abdallah ahmed
+print(mystringtwo)  # Output: iam abdallah ahmed
+
+
+mystringthree = "iam 'abdallah ahmed'"
+mystringfour = 'iam "abdallah ahmed"'
+
+print(mystringthree)  # Output: iam 'abdallah ahmed'
+print(mystringfour)  # Output: iam "abdallah ahmed"
+
+
+mystringfive = '''iam abdallah ahmed
+and i love python
+and i love programming'''
+mystringsix = """iam abdallah ahmed
+and i "love" python 
+and i love programming"""
+
+print(mystringfive)
+print(mystringsix)
